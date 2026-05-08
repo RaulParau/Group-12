b19fa56bfe28de839f8c127bfb7deb0f38fdddd5
Omer <96557499+OmericoN@users.noreply.github.com>
2026-05-08 10:16:16 +0200

    Test --> Validation with Confusion matrix (accuracy = 0.9997)

--- src/valid.py
+++ src/valid.py
@@ -1,24 +1,28 @@
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
-from config import CSV_DIR_TRAIN, IMG_DIR_TRAIN, BEST_MODEL
+from config import CSV_DIR_TRAIN, IMG_DIR_TRAIN, BEST_MODEL, param_config, BATCH_SIZE
 from sklearn.metrics import accuracy_score, confusion_matrix
 from loader import get_dataloaders
-from cnn import LeNet, EnhancedLeNet
+from cnn import LeNet, EnhancedLeNet, CustomResNet
 
 
 def test(model_path=BEST_MODEL):
-    _, _, dataloader_test = get_dataloaders(batch_size=16)
+    _, dataloader_val = get_dataloaders(batch_size=BATCH_SIZE)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     checkpoint = torch.load(model_path, map_location=device)
     config = checkpoint.get("config", {})
 
-    if config.get("model", "enhanced").lower() == "lenet":
+    if config["model"].lower() == "lenet":
         model = LeNet().to(device)
-    else:
+    elif config["model"].lower() == "enhancedlenet":
         model = EnhancedLeNet().to(device)
+    elif config["model"].lower() == "resnet":
+        model = CustomResNet().to(device)
+    else:
+        raise ValueError(f"Unsupported model: {config['model']}")
 
     model.load_state_dict(checkpoint["model_state_dict"])
     model.eval()
@@ -30,7 +34,7 @@ def test(model_path=BEST_MODEL):
     test_loss = 0
 
     with torch.no_grad():
-        for images, labels in dataloader_test:
+        for images, labels in dataloader_val:
             images, labels = images.to(device), labels.to(device)
 
             outputs = model(images)
@@ -46,7 +50,7 @@ def test(model_path=BEST_MODEL):
     acc = accuracy_score(all_labels, all_preds)
     cm = confusion_matrix(all_labels, all_preds)
 
-    print(f"Test Loss: {test_loss / len(dataloader_test):.4f}")
+    print(f"Test Loss: {test_loss / len(dataloader_val):.4f}")
     print(f"Test Accuracy: {acc:.4f}")
 
     plt.figure(figsize=(8, 6))
